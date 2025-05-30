edba94316563d7d417a5034401cba703be1a5eb0
fahimabir8 <fahimabir8@users.noreply.github.com>
2025-05-30 23:23:22 +0600

    robo

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+function setup(){
+    createCanvas(600,400);
+}
+
+function draw(){
+    background(220);
+    
+    rect(80,25,400,300)
+    rect(120,80,100,70)
+    rect(320,80,100,70)
+    rect(220,190,110,70)
+    
+}
