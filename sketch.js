5500f9044fa2ee1c6c08b76b4f27246df9d26e35
fahimabir8 <fahimabir8@users.noreply.github.com>
2025-05-31 23:18:22 +0600

    color done

--- sketch.js
+++ sketch.js
@@ -6,9 +6,18 @@ function setup(){
 function draw(){
     background(220);
     
+    fill(155,0,255)
     rect(80,25,400,300)
+
+    fill(255,0,0)
     rect(120,80,100,70)
     rect(320,80,100,70)
+    
+    fill(0,0,0)
+    stroke(0,0,255)
+    strokeWeight(14)
     rect(220,190,110,70)
+
+
     
 }
